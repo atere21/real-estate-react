4f48133762be70bbf403cca11c4c57a4476c87ad
atere21 <you@tosinateret@gmail.com>
2023-11-02 07:25:04 -0700

    mern

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Header>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+    </Header>
+  )
+}
