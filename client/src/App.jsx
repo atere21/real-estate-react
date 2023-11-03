dee1a5cd0900d05343350ae5b53779169344ae81
atere21 <you@tosinateret@gmail.com>
2023-11-03 06:04:00 -0700

    fullstack-realestate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,19 @@ import About from "./pages/About"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Home from "./pages/Home"
-import Header from "./components/Header"
+
+import HeaderBar from "./components/Header"
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <HeaderBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/about" element={<About />} /> {/* Use lowercase "about" */}
-        <Route path="/profile" element={<Profile />} /> {/* Use lowercase "profile" */}
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BiSearch } from 'react-icons/bi'
+import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function HeaderBar() {
   return (
-    <Header>
+    <header className='bg-slate-300 shadow-md '>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to="/">
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Sahand</span>
             <span className='text-slate-700'>Estate</span>
         </h1>
-    </Header>
-  )
+        </Link>
+
+        <form
+         className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input className='bg-transparent focus::outline-none w-24 sm:w-64'
+           type='text' placeholder='Search...'
+           />
+           <BiSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+          <Link to="/home"> 
+          <li className='hidden sm:inline text-slate-700 hover::underline cursor-pointer'>Home</li>
+          </Link>
+          <Link to="/about">
+          <li className='hidden sm:inline text-slate-700 hover::underline cursor-pointer'>About</li>
+          </Link>
+          <Link to="/signin">
+          <li className=' sm:inline text-slate-700 hover::underline cursor-pointer'>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
