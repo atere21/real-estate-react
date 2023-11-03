dba971ad863e0cc631124608862aa6a152111056
atere21 <you@tosinateret@gmail.com>
2023-11-03 08:10:55 -0700

    monodb connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+});
+
 const app = express();
 
-mongoose.connect("")
 app.listen(3000, () => {
     console.log('server is running on port 3000!');
 }
