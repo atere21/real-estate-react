56b6d31ce9aed2a1b2515f907c1699499853f96a
atere21 <you@tosinateret@gmail.com>
2023-11-03 10:05:37 -0700

    created a test-api-route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
 import express from 'express';
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js'
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('connected to MongoDB!');
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 })
-.catch((err) => {
-    console.log(err);
-});
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000!');
-}
-)
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
