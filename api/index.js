53ea338b146cc99d29f96b249ac136c256cf0610
atere21 <you@tosinateret@gmail.com>
2023-11-03 18:13:08 -0700

    signup

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
+  autoIndex: true,
 })
   .then(() => {
     console.log('Connected to MongoDB!');
@@ -18,9 +19,11 @@ mongoose.connect(process.env.MONGO, {
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
